69b785aa064947ede5a45242b9da7f524667ed53
themagmalord333-oss <the.magma.lord.333@gmail.com>
2026-07-21 13:43:49 +0530

    Update adminmention.py

--- Elevenyts/plugins/features/adminmention.py
+++ Elevenyts/plugins/features/adminmention.py
@@ -1,18 +1,4 @@
-# ==========================================================
-# Copyright (c) 2026 ArtistBots
-# All Rights Reserved.
-#
-# Project      : ArtistBots API Telegram Music Bot
-# Powered By   : Artist
-# Type         : API Based Telegram Music Bot
-#
-# Bot          : @ArtistApibot
-# Channel      : https://t.me/artistbots
-# GitHub       : https://github.com/elevenyts
-#
-# Unauthorized copying, modification, or redistribution
-# of this source code without permission is prohibited.
-# ==========================================================
+
 import re
 from pyrogram import filters, types, enums
 
