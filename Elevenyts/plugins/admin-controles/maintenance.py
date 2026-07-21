880bf69e0911c62ce520d7352a5c6abed0884014
themagmalord333-oss <the.magma.lord.333@gmail.com>
2026-07-21 15:44:52 +0530

    Update maintenance.py

--- Elevenyts/plugins/admin-controles/maintenance.py
+++ Elevenyts/plugins/admin-controles/maintenance.py
@@ -1,6 +1,6 @@
 from pyrogram import filters, types
 
-from Elevenyts import app, db, lang
+from Anysnap import app, db, lang
 
 
 @app.on_message(filters.command(["maintenance"]) & app.sudo_filter)
@@ -12,12 +12,12 @@ async def _maintenance(_, m: types.Message):
         await m.delete()
     except Exception:
         pass
-    
+
     # If no argument, show current status
     if len(m.command) < 2:
         status = await db.get_maintenance()
         status_text = "🔴 ᴇɴᴀʙʟᴇᴅ" if status else "🟢 ᴅɪꜱᴀʙʟᴇᴅ"
-        
+
         await m.reply_text(
             f"<blockquote><u><b>🔧 ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ᴍᴏᴅᴇ ꜱᴛᴀᴛᴜꜱ</b></u>\n\n"
             f"<b>ᴄᴜʀʀᴇɴᴛ ꜱᴛᴀᴛᴜꜱ:</b> {status_text}\n\n"
@@ -26,9 +26,9 @@ async def _maintenance(_, m: types.Message):
             f"<code>/maintenance disable</code> - Disable mode</blockquote>"
         )
         return
-    
+
     mode = m.command[1].lower()
-    
+
     if mode in ["enable", "on", "1", "true"]:
         await db.set_maintenance(True)
         await m.reply_text(
@@ -36,14 +36,14 @@ async def _maintenance(_, m: types.Message):
             "ᴏɴʟʏ ꜱᴜᴅᴏ ᴜꜱᴇʀꜱ ᴄᴀɴ ᴜꜱᴇ ᴛʜᴇ ʙᴏᴛ ɴᴏᴡ.\n"
             "ʀᴇɢᴜʟᴀʀ ᴜꜱᴇʀꜱ ᴡɪʟʟ ꜱᴇᴇ ᴀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ᴍᴇꜱꜱᴀɢᴇ.</blockquote>"
         )
-        
+
     elif mode in ["disable", "off", "0", "false"]:
         await db.set_maintenance(False)
         await m.reply_text(
             "<blockquote><u><b>🟢 ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ᴍᴏᴅᴇ ᴅɪꜱᴀʙʟᴇᴅ</b></u>\n\n"
             "ᴛʜᴇ ʙᴏᴛ ɪꜱ ɴᴏᴡ ᴀᴠᴀɪʟᴀʙʟᴇ ꜰᴏʀ ᴀʟʟ ᴜꜱᴇʀꜱ.</blockquote>"
         )
-        
+
     else:
         await m.reply_text(
             "<blockquote>❌ <b>ɪɴᴠᴀʟɪᴅ ᴏᴘᴛɪᴏɴ</b>\n\n"
