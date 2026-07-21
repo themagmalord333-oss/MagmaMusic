6c11bef1df5056bafe54eb373f644f162a35e9a8
themagmalord333-oss <the.magma.lord.333@gmail.com>
2026-07-21 13:30:57 +0530

    Update __init__.py

--- Elevenyts/helpers/__init__.py
+++ Elevenyts/helpers/__init__.py
@@ -1,18 +1,4 @@
-# ==========================================================
-# Copyright (c) 2026 ArtistBots
-# All Rights Reserved.
-#
-# Project      : ArtistBots API Telegram Music Bot
-# Powered By   : Artist
-# Type         : API Based Telegram Music Bot
-#
-# Bot          : @ArtistApibot
-# Channel      : https://t.me/artistbots
-# GitHub       : https://github.com/elevenyts
-#
-# Unauthorized copying, modification, or redistribution
-# of this source code without permission is prohibited.
-# ==========================================================
+
 from ._admins import admin_check, can_manage_vc, is_admin, reload_admins, can_manage_vc_channel
 from ._dataclass import Media, Track
 from ._exec import format_exception, meval
